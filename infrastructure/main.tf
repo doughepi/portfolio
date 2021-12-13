6c906547ec02afebb96e46fb36152090d7a663aa
Piper Dougherty <doughertypiper@gmail.com>
2021-12-12 23:14:19 -0600

    Remove website bit.

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -4,12 +4,6 @@ resource "google_storage_bucket" "portfolio" {
   force_destroy = true
 
   uniform_bucket_level_access = true
-
-  website {
-    main_page_suffix = "index.html"
-    not_found_page   = "404.html"
-  }
-
 }
 
 resource "google_storage_bucket_iam_member" "portfolio" {
